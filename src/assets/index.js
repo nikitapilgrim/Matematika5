90c6f02c86d936e82cda0e717e25ed9d6329809d
nikitapilgrim <nikita.pillgrim@icloud.com>
2020-02-10 20:46:43 +0700

    preloader and tutorial fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,22 +2,6 @@ import tutorialAssets from "./image/tutorial";
 
 const assets = [
     {
-        name: 'svg_help',
-        url: require('./svg/help.svg')
-    },
-    {
-        name: 'svg_menu',
-        url: require('./svg/menu.svg')
-    },
-    {
-        name: 'svg_volume-off',
-        url: require('./svg/volume_off.svg')
-    },
-    {
-        name: 'svg_volume-on',
-        url: require('./svg/volume_on.svg')
-    },
-    {
         name: 'sound',
         url: require('./sound/music.mp3')
     },
