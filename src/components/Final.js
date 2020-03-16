79920cae82789f80148a5b6eba634c77662c1057
nikitapilgrim <nikita.pillgrim@icloud.com>
2020-03-16 18:19:44 +0300

    quotes add

--- src/components/Final.js
+++ src/components/Final.js
@@ -4,12 +4,16 @@ import useStoreon from "storeon/react";
 import {Scale} from "./Animate/Slide";
 import menuobjects from "../assets/image/final/final-stage-background.png";
 import button from "../assets/image/final/final-stage-button.png";
+import quote1 from "../assets/image/final/quote1.png";
+import quote2 from "../assets/image/final/quote2.png";
+import quote3 from "../assets/image/final/quote3.png";
 import {Sound} from "./Sound";
 import {MenuWithouModal} from "./Menu";
 import {sounds} from "../sounds";
 import useKeyPressEvent from "react-use/lib/useKeyPressEvent";
 import useClickAway from "react-use/lib/useClickAway";
-import {eventDesk} from "./Desk";
+
+const quotes = [quote1, quote2, quote3];
 
 const Wrapper = styled.div`
   position: fixed;
@@ -109,9 +113,36 @@ const FakeButton = styled.button`
   }
 `;
 
+
+const QuotesWrapper = styled.div`
+  position: absolute;
+  max-width: 20rem;
+  right: -15rem;
+  @media (max-width: 500px) {
+    display: none;
+  }
+  img {
+    display: block;
+    max-width: 100%;
+  }
+`
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
 export const Final = () => {
     const {dispatch, stage, deskRef, final, showDesk, modal, kviz} = useStoreon('stage', 'countQuestions', 'showDesk', 'final', 'modal', 'deskRef', 'kviz');
     const [show, setShow] = useState(final);
+    const [quote, setQuote] = useState(quotes[randomInteger(0, quotes.length - 1)]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setQuote(quotes[randomInteger(0, quotes.length - 1)]);
+        }, 2000);
+        return () => clearInterval(interval);
+    },[]);
 
     useEffect(() => {
         if (final) {
@@ -160,6 +191,9 @@ export const Final = () => {
             <MenuObjectsWrapper>
                 <img src={menuobjects} alt=""/>
             </MenuObjectsWrapper>
+            <QuotesWrapper>
+                <img src={quote} alt=""/>
+            </QuotesWrapper>
             <Buttons>
 
                 <HiddenWrapp hide={false}>
